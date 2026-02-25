5f958c4f7c7b90b775d2e49829aee2677db88568
Olga Kruglova <olha.kruhlova@inetum.com>
2026-02-25 12:07:11 +0100

    add music animation

---
+++ UI/src/app/music-animation/music-animation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-music-animation",
+  templateUrl: "./music-animation.component.html",
+  styleUrls: ["./music-animation.component.scss"],
+  standalone: true
+})
+export class MusicAnimationComponent {}
